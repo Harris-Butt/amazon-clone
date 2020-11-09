5900cef5fff7c6c6d3ba67cc88131b9705aca31b
Harris-Butt <33513710+Harris-Butt@users.noreply.github.com>
2020-11-09 23:25:23 +0500

    finish front page

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuMkq_KXHzHICBPLaPL5ALzyWubRsUskc",
+    authDomain: "clone-b88ad.firebaseapp.com",
+    databaseURL: "https://clone-b88ad.firebaseio.com",
+    projectId: "clone-b88ad",
+    storageBucket: "clone-b88ad.appspot.com",
+    messagingSenderId: "75937547293",
+    appId: "1:75937547293:web:b0fab9b0dac14aa7f7b2f4"
+  };
